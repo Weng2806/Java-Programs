0789bda8343a0c329302bc83d9b41d4d8c8e5431
Ruel Pamaran <147482226+weng2806@users.noreply.github.com>
2024-04-20 15:29:51 +0700

    Create zTabingi.java

---
+++ zTabingi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class zTabingi {
+    public static void main(String[] args) throws Exception {
+    Scanner input = new Scanner(System.in);
+        
+        System.out.print("Please enter the size: ");
+        int size = input.nextInt();
+        int col = 1;
+
+        for (int row = 1; row <= size; row++) { // patayo to
+            for (int j = 1; j <= size; j++) { // merong pahiga pero hindi natatanggal sa git
+                if (row == 1 || row == size || row == j) {
+                System.out.print("# ");
+                }
+
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
